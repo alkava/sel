bb8d48b238c5074804a52786ce2a0636036ac263
Anna Bondarenko <anna.kovsh@gmail.com>
2017-01-25 19:40:14 +0300

    Init commit

---
+++ test/test_first.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+def test_first():
+    wd = webdriver.Chrome()
+    wd.get("http://www.google.com/")
+    wait = WebDriverWait(wd, 10)
+    wait.until(EC.visibility_of_element_located((By.ID, 'sb_ifc0')))
+    wd.quit
+    print("the end.")
+
